af9b7c49e17c2da6552757463e58d18180945648
Alejandro <52725111+Alejandro-prog@users.noreply.github.com>
2021-10-23 18:23:17 -0500

    👓Esta mamada que?

--- ApiHealthTech/settings.py
+++ ApiHealthTech/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
