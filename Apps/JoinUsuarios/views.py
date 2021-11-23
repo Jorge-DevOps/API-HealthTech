178bd01355fe7bf092022d18adadeb1f4995f54f
Maruan Arias <maruannn.a@hotmail.com>
2021-11-23 14:52:16 -0500

    Validacion Login tipo de usuario

--- Apps/JoinUsuarios/views.py
+++ Apps/JoinUsuarios/views.py
@@ -10,7 +10,7 @@ import xlwt
 class Jointableapi(viewsets.ModelViewSet):
     queryset=JoinTablesmodel.objects.raw('SELECT id_usuario, tipo_documento, numero_documento, username, password, email, telefono, sexo, fecha_nacimiento, grupo_sanguineo, estrato, estado_civil, id_perfil FROM  administrador UNION SELECT  id_usuario, tipo_documento, numero_documento, username, password, email, telefono, sexo, fecha_nacimiento, grupo_sanguineo, estrato, estado_civil, id_perfil FROM medico UNION SELECT  id_usuario, tipo_documento, numero_documento, username, password, email, telefono, sexo, fecha_nacimiento, grupo_sanguineo, estrato, estado_civil, id_perfil FROM paciente')
     serializer_class = Jointableserializer
-
+   
 
 
 def export_excel(request):
--- Apps/share/cita/models.py
+++ Apps/share/cita/models.py
@@ -1,8 +1,26 @@
 from django.db import models
 from Apps.agenda.models import Agenda
+#from Apps.share.medico.models import Medico
 from Apps.share.paciente.models import Paciente
 from Apps.horario.models import horario
+from django.db.models import Q, query
+from django.db import connection
+from django.http import HttpResponse
 # Create your models here.
+#Horario= horario.objects.filter(Q(hora_inicio=query)|Q())
+
+def traerMedicos(request):
+    
+    with connection.cursor() as cursor:
+            cursor.execute("SELECT  username, id_agenda FROM  medico ")
+            rawData = cursor.fetchall()
+            result = []
+            for r in rawData:
+                result.append(list(r))
+                rows = result        
+    response = HttpResponse(content_type='application/json')
+    return result
+
 class Cita(models.Model):
     id_cita = models.AutoField(primary_key=True)
     id_agenda = models.ForeignKey(
@@ -12,7 +30,8 @@ class Cita(models.Model):
     fecha = models.DateField()
     id_horario = models.ForeignKey(
         horario, models.DO_NOTHING, db_column='id_horario')
-
+    Medico=traerMedicos
+    #medicos = models.ManyToManyField(Medico)
     class Meta:
         managed = False
         db_table = 'cita'
